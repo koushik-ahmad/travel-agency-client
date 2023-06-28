ec2347b404b7202b2990f9e6bff1c80b2d4427b0
koushik-ahmad <koushikahmad123@gmail.com>
2023-06-28 13:44:09 +0600

    first commit

---
+++ src/components/Destination/Destination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Destination = () => {
+    return (
+        <div>
+            <h2>This destination</h2>
+        </div>
+    );
+};
+
+export default Destination;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAVF4cxlf8C4AW4CVG2g-XTLS5hhkdkku8",
+    authDomain: "cs-authentication-b6-b582b.firebaseapp.com",
+    projectId: "cs-authentication-b6-b582b",
+    storageBucket: "cs-authentication-b6-b582b.appspot.com",
+    messagingSenderId: "286703297525",
+    appId: "1:286703297525:web:a886921c14cb5e578af6bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
